2d839d8e38214bc1b8dd88782a36858da61628ac
Efrain Sanchez <109004691+EfrainMaster@users.noreply.github.com>
2023-05-15 21:32:14 -0300

    new

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const miForm = document.getElementById("formCont");
 miForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    // alert("Formulario Enviado");
     let imagen = '<img src= "img/ok.com.png" width=30 height=30 >'
     document.getElementById("mensaje").innerHTML = "El Formulario se Completo con Exito" + imagen;
 })
+
 
